6470d3dcc5c8cbd2de83e18466d946885221e669
Bjorn <bp@datakortet.no>
2019-03-13 12:31:22 +0100

    Move commands to their own file.

--- dkbuild_apacheconf/__main__.py
+++ dkbuild_apacheconf/__main__.py
@@ -1,46 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-import re
 import sys
-import textwrap
-from jinja2 import Template
-from dkfileutils.path import Path
 
-from .context import Context
-from . import __version__, __doc__ as module_doc
+from . import __version__, __doc__ as module_doc, cli_commands
 import argparse
 
 
-DIRNAME = Path(__file__).dirname()
-APACHECONF_TEMPLATE = DIRNAME / 'apache.conf.jinja'
-
-
-def init_cmd(argv, args):
-    site_ini = Path('site.ini')
-    if site_ini.exists() and not args.force:
-        print(textwrap.dedent("""
-            ERROR: site.ini exists
-        """),  file=sys.stderr)
-        sys.exit(1)
-    else:
-        site_ini.write(DIRNAME / 'site.ini')
-        if args.verbose:
-            print((DIRNAME / 'site.ini').read())
-
-
-def run_cmd(argv, args):
-    ctx = Context(argv, args)
-    t = Template(APACHECONF_TEMPLATE.read())
-    txt = ctx.render(t)
-    if args.dry:
-        print(txt)
-    else:
-        apache_conf = Path(args.out)
-        apache_conf.write(txt)
-
-
 def main():
-    p = argparse.ArgumentParser(description=module_doc)
+    p = argparse.ArgumentParser(
+        description=module_doc
+    )
     p.add_argument('-V', '--version', action='version', version="%(prog)s v" + __version__)
     p.add_argument('-v', '--verbose', action='count', default=0, help='increase verbosity')
     p.add_argument('-f', '--force', action='store_true', help='force operation')
@@ -60,7 +29,7 @@ def main():
         print(args, argv)
 
     if args.command:
-        globals()[args.command + '_cmd'](argv, args)
+        getattr(cli_commands, args.command + '_cmd')(argv, args)
 
     sys.exit(0)
 
---
+++ dkbuild_apacheconf/cli_commands.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+import sys
+import textwrap
+
+from dkfileutils.path import Path
+from jinja2 import Template
+
+from dkbuild_apacheconf.context import Context
+
+DIRNAME = Path(__file__).dirname()
+APACHECONF_TEMPLATE = DIRNAME / 'apache.conf.jinja'
+
+
+def init_cmd(argv, args):
+    site_ini = Path('site.ini')
+    if site_ini.exists() and not args.force:
+        print(textwrap.dedent("""
+            ERROR: site.ini exists
+        """),  file=sys.stderr)
+        sys.exit(1)
+    else:
+        site_ini.write(DIRNAME / 'site.ini')
+        if args.verbose:
+            print((DIRNAME / 'site.ini').read())
+
+
+def run_cmd(argv, args):
+    ctx = Context(argv, args)
+    t = Template(APACHECONF_TEMPLATE.read())
+    txt = ctx.render(t)
+    if args.dry:
+        print(txt)
+    else:
+        apache_conf = Path(args.out)
+        apache_conf.write(txt)
