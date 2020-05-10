cb1ff009e79f028d6c3db5f68e2c41d9db73a8e9
thebjorn <bp@norsktest.no>
2020-05-10 15:55:48 +0200

    write sample site.ini without clobbering existing site.ini.

--- dkbuild_apacheconf/cli_commands.py
+++ dkbuild_apacheconf/cli_commands.py
@@ -10,27 +10,32 @@ from dkbuild_apacheconf.context import Context
 
 DIRNAME = Path(__file__).dirname()
 APACHECONF_TEMPLATE = DIRNAME / 'apache.conf.jinja'
+SITE_INI_TEMPLATE = DIRNAME / 'site.ini'
 
 
 def init_cmd(argv, args):
     site_ini = Path('site.ini')
     if site_ini.exists() and not args.force:
         print(textwrap.dedent("""
-            ERROR: site.ini exists
+            ERROR: site.ini exists, writing to site.ini.new
         """),  file=sys.stderr)
-        sys.exit(1)
-    else:
-        site_ini.write(DIRNAME / 'site.ini')
-        if args.verbose:
-            print((DIRNAME / 'site.ini').read())
+        site_ini = Path('site.ini.new')
+
+    site_ini.write(SITE_INI_TEMPLATE.read())
+    if args.verbose or True:
+        print(SITE_INI_TEMPLATE.read())
 
 
 def run_cmd(argv, args):
     """Create a new apache.conf file from site.ini and server.ini
     """
     ctx = Context(argv, args)
-    t = Template(APACHECONF_TEMPLATE.read())
+    template_text = APACHECONF_TEMPLATE.read()
+    t = Template(template_text)
     txt = ctx.render(t)
+    # print('start')
+    # print(txt)
+    # print('end')
     if args.dry:
         print(txt)
     else:
